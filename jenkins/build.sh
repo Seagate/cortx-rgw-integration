41d62d88b17e92fa49e06d647d03fa5365f00f5b
Palak Garg <palak.garg@seagate.com>
2022-02-07 01:45:07 -0700

    Fix code issue
    Signed-off-by: Palak Garg <palak.garg@seagate.com>

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -61,7 +61,7 @@ rpm -q rpm-build > /dev/null || {
 echo $VER > "$BASE_DIR"/VERSION
 /bin/chmod +rx "$BASE_DIR"/VERSION
 /bin/chmod +x "$BASE_DIR"/src/setup/rgw_setup
-/bin/chmod +x "$BASE_DIR"/src/support/rgw_support_bundle
+#/bin/chmod +x "$BASE_DIR"/src/support/rgw_support_bundle
 
 INSTALL_PATH="/opt/seagate/""${PRODUCT}"
 
