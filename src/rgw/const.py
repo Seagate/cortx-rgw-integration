37e741a572b7fc41d0a8d4c333e933031ff2bcc3
Sachitanand Shelake <65209830+sachitanands@users.noreply.github.com>
2022-03-30 09:10:34 +0530

    CORTX-29705:RGW:updation of rgw service name as rgw -> rgw_s3 in motr client & --services parameters (#65)
    * changes in cluster.yaml & in GConf about rgw service name
    
    Signed-off-by: Sachitanand Shelake <sachitanand.shelake@seagate.com>
    
    * added check for servicename=all
    
    Signed-off-by: Sachitanand Shelake <sachitanand.shelake@seagate.com>
    
    * typo fix
    
    Signed-off-by: Sachitanand Shelake <sachitanand.shelake@seagate.com>
    
    * review comment changes
    
    Signed-off-by: Sachitanand Shelake <sachitanand.shelake@seagate.com>
    
    Co-authored-by: Rahul Tripathi <78896910+rahul-tripathi-git@users.noreply.github.com>

--- src/rgw/const.py
+++ src/rgw/const.py
@@ -17,6 +17,7 @@
 from enum import Enum
 
 COMPONENT_NAME = 'rgw'
+COMPONENT_SVC_NAME = 'rgw_s3'
 DECRYPTION_KEY = 'cortx'
 SERVICE_NAME = f'{COMPONENT_NAME}_setup' # rgw_setup
 INSTALL_PATH = '/opt/seagate/cortx'
