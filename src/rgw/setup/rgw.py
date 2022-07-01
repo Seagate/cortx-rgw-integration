9cca02be2e049a053c36c55452c7780d63580147
Sachitanand Shelake <sachitanand.shelake@seagate.com>
2022-07-01 01:37:50 -0600

    addressing review comments
    Signed-off-by: Sachitanand Shelake <sachitanand.shelake@seagate.com>

--- src/rgw/setup/rgw.py
+++ src/rgw/setup/rgw.py
@@ -397,30 +397,32 @@ class Rgw:
 
         # Adding retry logic for user creation with given timeout value.
         retry_count = 0
+        return_code = -1
         while(retry_count < const.USER_CREATION_MAX_RETRY_COUNT):
             _, err, rc, = SimpleProcess(create_usr_cmd).run(timeout=const.ADMIN_CREATION_TIMEOUT)
             if rc == 0:
                 Log.info(f'RGW admin user {user_name} is created.')
-                return 0
-            elif rc != 0:
+                return_code = 0
+            else:
                 err = err.decode(const.UTF_ENCODING) if isinstance(err, bytes) else err
                 if err_str in err:
                     Log.info(f'RGW admin user {user_name} is already created,'
                         ' Skipping user creation.')
-                    return 0
+                    return_code = 0
                 elif timeout_str in err:
                     Log.info('RGW user creation process exceeding timeout value - '
-                        f'{const.ADMIN_CREATION_TIMEOUT} seconds. Skipping user creation on this node.')
+                        f'{const.ADMIN_CREATION_TIMEOUT} seconds. Retrying user creation on this node.')
                     retry_count = retry_count + 1
                     continue
                 # Break retry loop after max retries.
                 elif retry_count == const.USER_CREATION_MAX_RETRY_COUNT:
-                    Log.info('User creation reties exceeded than max allowed value i.e .'
-                        f'{const.USER_CREATION_MAX_RETRY_COUNT} . Skipping user creation on this node.')
-                    return rc
+                    Log.info('User creation retries exceeded than max allowed value '
+                        f'{const.USER_CREATION_MAX_RETRY_COUNT}. Skipping user creation on this node.')
+                    return_code = rc
                 else:
                     Log.error(f'"{create_usr_cmd}" failed with error {err}.')
-                    return rc
+                    return_code = rc
+        return return_code
 
     @staticmethod
     def _parse_endpoint_values(conf: MappedConf, instance: int, client_instance_count: int, svc_name: str):
